a1895173e16aa01a38d05ae77fe19333f20c1acf
Shuvendu Moharana <98461609+shuvendu098@users.noreply.github.com>
2023-03-19 01:28:13 +0530

    login page

--- app/src/main/java/com/example/fooddelivery/MainActivity.java
+++ app/src/main/java/com/example/fooddelivery/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.fooddelivery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.window.SplashScreen;
 
 public class MainActivity extends AppCompatActivity {
-
+	private static int SPLASH_SCREEN=5000;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_main);
+		if (getSupportActionBar() != null) {
+			getSupportActionBar().hide();
+		}
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				Intent intent =new Intent(MainActivity.this, login.class);
+				startActivity(intent);
+				finish();
+			}
+		}, SPLASH_SCREEN);
 	}
 }
---
+++ app/src/main/java/com/example/fooddelivery/newUser.java
@@ -0,0 +1,14 @@
+package com.example.fooddelivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class newUser extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_new_user);
+	}
+}
